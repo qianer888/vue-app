d838b545dc69c03494d9e3b13601d7bad32d3cdb
Qianer <1352407360@qq.com>
2019-07-30 10:58:55 +0800

    rem适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 插件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible/index.min.js'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
